fc6fa91f19dd584cc99b16e2f817d67a3be95407
Muhammad Nizar <mnizarzr@gmail.com>
2024-07-03 17:37:35 +0700

    chore: Update tsconfig compiler options

--- src/pages/admin/EditCar.tsx
+++ src/pages/admin/EditCar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import CarForm from "@/components/admin/CarForm";
 import axios from "@/utils/axios";
 import { useParams } from "react-router-dom";
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "node:path"
 import tsConfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
